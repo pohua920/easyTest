1106861a790e8506431052cad7c6fbc2ebd46475
pohua920 <pohua920@gmail.com>
2024-08-06 10:08:28 +0800

    version 0

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,9 @@
+insert into person (emplid, first_name, last_name) 
+values (null, 'Bruce', 'Phillips');
+
+insert into person (emplid, first_name, last_name) 
+values (null, 'Kit2', 'Cole');
+
+insert into person (emplid, first_name, last_name) 
+values (null, 'Frank2', 'Sorio');
+
---
+++ target/test-classes/test-data.sql
@@ -0,0 +1,9 @@
+insert into person (emplid, first_name, last_name) 
+values (null, 'Bruce', 'Phillips');
+
+insert into person (emplid, first_name, last_name) 
+values (null, 'Kit3', 'Cole');
+
+insert into person (emplid, first_name, last_name) 
+values (null, 'Frank3', 'Sorio');
+
